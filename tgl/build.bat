a7b72d09adae9389adc41c0e20338f57ba42e7c5
fosobiy <89915056+fosobiy@users.noreply.github.com>
2025-01-23 15:40:17 +0300

    Добавил TinyGL для KolibriOS

---
+++ tgl/build.bat
@@ -0,0 +1,25 @@
+..\ktcc\kos32-tcc -Iinclude src\api.c -c -o obj\api.o
+..\ktcc\kos32-tcc -Iinclude src\arrays.c -c -o obj\arrays.o
+..\ktcc\kos32-tcc -Iinclude src\clear.c -c -o obj\clear.o
+..\ktcc\kos32-tcc -Iinclude src\clip.c -c -o obj\clip.o
+..\ktcc\kos32-tcc -Iinclude src\error.c -c -o obj\error.o
+..\ktcc\kos32-tcc -Iinclude src\get.c -c -o obj\get.o
+..\ktcc\kos32-tcc -Iinclude src\glu.c -c -o obj\glu.o
+..\ktcc\kos32-tcc -Iinclude src\image_util.c -c -o obj\image_util.o
+..\ktcc\kos32-tcc -Iinclude src\init.c -c -o obj\init.o
+..\ktcc\kos32-tcc -Iinclude src\kosgl.c -c -o obj\kosgl.o
+..\ktcc\kos32-tcc -Iinclude src\light.c -c -o obj\light.o
+..\ktcc\kos32-tcc -Iinclude src\list.c -c -o obj\list.o
+..\ktcc\kos32-tcc -Iinclude src\matrix.c -c -o obj\matrix.o
+..\ktcc\kos32-tcc -Iinclude src\misc.c -c -o obj\misc.o
+..\ktcc\kos32-tcc -Iinclude src\msghandling.c -c -o obj\msghandling.o
+..\ktcc\kos32-tcc -Iinclude src\select.c -c -o obj\select.o
+..\ktcc\kos32-tcc -Iinclude src\specbuf.c -c -o obj\specbuf.o
+..\ktcc\kos32-tcc -Iinclude src\texture.c -c -o obj\texture.o
+..\ktcc\kos32-tcc -Iinclude src\vertex.c -c -o obj\vertex.o
+..\ktcc\kos32-tcc -Iinclude src\zbuffer.c -c -o obj\zbuffer.o
+..\ktcc\kos32-tcc -Iinclude src\zdither.c -c -o obj\zdither.o
+..\ktcc\kos32-tcc -Iinclude src\zline.c -c -o obj\zline.o
+..\ktcc\kos32-tcc -Iinclude src\zmath.c -c -o obj\zmath.o
+..\ktcc\kos32-tcc -Iinclude src\ztriangle.c -c -o obj\ztriangle.o
+::pause
